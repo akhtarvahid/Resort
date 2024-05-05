a88beabaff14a601048be5d9e92fe0e8fe08c50e
Vahid Akhtar <akhtarvahid543@gmail.com>
2024-05-06 00:05:43 +0530

    gallary component changed

--- src/components/home/Gallary.js
+++ src/components/home/Gallary.js
@@ -1,40 +1,59 @@
-import React from 'react';
-import Title from '../common/Title';
-import Carousel from '../common/Carousel';
-import { galleryData } from '../../utils/gallary-data';
+import React from "react";
+import Title from "../common/Title";
+import Carousel from "../common/Carousel";
+import { galleryData } from "../../utils/gallery-data";
 
-class Gallary extends React.Component{
-    state={
-      gallary: galleryData
-    }
-    render(){
-    return (
-        <div>
-         <Title title="Gallary" center/>  
-         <div style={{margin:50}}>
-         <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
-            <div className="carousel-inner">
-                {this.state.gallary.map(({id, img, className, classImg, title})=>
-                    <Carousel 
-                    key={id}
-                    url={img} className={className}
-                    classImg={classImg}
-                    imgStyle={{height:'800px', objectFit:'cover'}}
-                    altName={title}
-                />)}
-            </div>
-            <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-              <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span className="sr-only">Previous</span>
-            </a>
-            <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next"> 
-              <span className="carousel-control-next-icon" aria-hidden="true"></span>
-              <span className="sr-only">Next</span>
-            </a>
-        </div>   
+const Gallery = () => {
+  const [galleries] = React.useState(galleryData);
+
+  return (
+    <div>
+      <Title title="Gallery" center />
+      <div style={{ margin: 50 }}>
+        <div
+          id="carouselExampleControls"
+          className="carousel slide"
+          data-ride="carousel"
+        >
+          <div className="carousel-inner">
+            {galleries.map(({ id, img, className, classImg, title }) => (
+              <Carousel
+                key={id}
+                url={img}
+                className={className}
+                classImg={classImg}
+                imgStyle={{ height: "800px", objectFit: "cover" }}
+                altName={title}
+              />
+            ))}
+          </div>
+          <a
+            className="carousel-control-prev"
+            href="#carouselExampleControls"
+            role="button"
+            data-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="sr-only">Previous</span>
+          </a>
+          <a
+            className="carousel-control-next"
+            href="#carouselExampleControls"
+            role="button"
+            data-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="sr-only">Next</span>
+          </a>
         </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 };
-}
-export default Gallary;
+export default Gallery;
