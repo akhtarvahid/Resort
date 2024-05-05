c7ecfa4f52bab28acd6a0b6134da3beb88c60f19
Vahid Akhtar <akhtarvahid543@gmail.com>
2024-05-05 10:23:38 +0530

    about migrated

---
+++ src/components/About/About.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Section from "../common/Section";
+import Title from "../common/Title";
+import aboutImg from "../../images/aboutBcg.jpeg";
+import { PrimaryBtn } from "../common/Buttons";
+import { AboutCenter } from "./About.styled";
+
+const About = () => {
+  return (
+    <Section>
+      <AboutCenter>
+        <div className="about-img">
+          <img src={aboutImg} alt="about us" />
+        </div>
+        <div className="about-info">
+          <Title title="about us" />
+          <p>
+            From its personalised service and sophisticated cuisine to its
+            distinctive design, authentic culture and calming spa, our Maldives
+            luxury resort works hard to tailor the experience to your needs. We
+            can orchestrate customised cruises to neighbouring islands and
+            beaches; plan private dinners at secluded locations; arrange yoga,
+            diving or snorkelling; and create adventures for families travelling
+            together.
+          </p>
+          <PrimaryBtn>read more</PrimaryBtn>
+        </div>
+      </AboutCenter>
+    </Section>
+  );
+};
+
+export default About;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from '../components/home/Header';
-import About from '../components/home/About';
+import About from '../components/About/About';
 import Rooms from '../components/home/Rooms';
 import Footer from './../components/home/Footer';
 import Gallary from '../components/home/Gallary';
