51d78a6c7f87a71878efad903ab0c5561197dc4e
Vahid Akhtar <akhtarvahid543@gmail.com>
2024-05-06 17:56:14 +0530

    banner and hero component

--- src/components/common/Hero.js
+++ src/components/common/Hero.js
@@ -1,11 +1,10 @@
-import styled from 'styled-components';
-// import homeImage from '../../images/homeBcg.jpeg'
-import { setFlex, setBackground } from '../../styles';
+import styled from "styled-components";
+import { setFlex, setBackground } from "../../styles";
 
 const Hero = styled.header`
- min-height: 100vh;
- ${props => setBackground({ img: props.img, color: "rgba(0,0,0,0)" })};
- ${setFlex({x: "center", y: "center"})}
-`
+  min-height: 100vh;
+  ${(props) => setBackground({ img: props.img, color: "rgba(0,0,0,0)" })};
+  ${setFlex({ x: "center", y: "center" })}
+`;
 
 export default Hero;
