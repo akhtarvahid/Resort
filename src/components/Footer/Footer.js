1f3745f047e4156b2de1710c211dc494f8b3904b
Vahid Akhtar <akhtarvahid543@gmail.com>
2024-05-09 23:24:16 +0530

    footer styled

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,27 +1,6 @@
 import React from 'react';
-import styled from 'styled-components';
+import { MyFooter } from './Footer.styled';
 
-const MyFooter = styled.div`
-    padding: 10px;
-    background: #626d6f;
-    color: white;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: 25px;
-    font-weight: 800;
-    font-family: cursive;
-    a{
-      img{
-          height:50px;
-          border-radius: 50%;
-          margin: 3px;
-      }
-    }
-    a:nth-child(2) img, a:nth-child(3) img, a:nth-child(4) img {
-        height: 50px;
-    }
-`
 
 const Footer = () => {
     return (
