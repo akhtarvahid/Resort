8ee438a322ed16cc2baf4f38d3d4a426fcf798c3
Vahid Akhtar <akhtarvahid543@gmail.com>
2024-05-09 23:37:04 +0530

    footer icons

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,25 +1,43 @@
-import React from 'react';
-import { MyFooter } from './Footer.styled';
-
+import React from "react";
+import { MyFooter } from "./Footer.styled";
+const linkedin = require("../../images/footer/linkedin.png");
+const stackOverflow = require("../../images/footer/so.png");
+const github = require("../../images/footer/github.png");
+const codeCamp = require("../../images/footer/codecamp.jpeg");
 
 const Footer = () => {
-    return (
-        <MyFooter>
-           <a href="https://www.linkedin.com/in/vaheed-akhtar-97a761a4/"
-           target="_blank" rel="noopener noreferrer">
-             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQguOImIJHOHUPczYNEPe5WHSlRUgiyXJ99lA&usqp=CAU" alt="LI-icon"/>
-           </a>
-           <a href="https://stackoverflow.com/users/6544460/vahid-akhtar?tab=profile" target="_blank" rel="noopener noreferrer">
-             <img src="https://icon-library.net/images/stackoverflow-icon/stackoverflow-icon-6.jpg" alt="so-icon"/>  
-           </a>
-           <a href="https://github.com/akhtarvahid" target="_blank" rel="noopener noreferrer">
-               <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="gh-icon"/>
-           </a>
-           <a href="https://www.freecodecamp.org/akhtarvahid" target="_blank" rel="noopener noreferrer">
-               <img src="http://static.libsyn.com/p/assets/2/f/f/7/2ff7cc8aa33fe438/freecodecamp-square-logo-large-1400.jpg" alt="fcc-icon"/>
-           </a>
-        </MyFooter>
-    );
+  return (
+    <MyFooter>
+      <a
+        href="https://www.linkedin.com/in/vaheed-akhtar-97a761a4/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src={linkedin} alt="LI-icon" />
+      </a>
+      <a
+        href="https://stackoverflow.com/users/6544460/vahid-akhtar?tab=profile"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src={stackOverflow} alt="so-icon" />
+      </a>
+      <a
+        href="https://github.com/akhtarvahid"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src={github} alt="gh-icon" />
+      </a>
+      <a
+        href="https://www.freecodecamp.org/akhtarvahid"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src={codeCamp} alt="fcc-icon" />
+      </a>
+    </MyFooter>
+  );
 };
 
 export default Footer;
