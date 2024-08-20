65b79e9857b5a361495a3d7e4c603e5b6accb1f4
Vahid Akhtar <akhtarvahid543@gmail.com>
2024-08-20 21:26:02 +0530

    added module to test file

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
- import ReactDOM from 'react-dom';
- import App from './App';
+import ReactDOM from 'react-dom';
+import App from './App';
+import Zoom from "react-medium-image-zoom";
+
 
  it('renders without crashing', () => {
    const div = document.createElement('div');
