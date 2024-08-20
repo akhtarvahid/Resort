b7e908e971979e8d04bb82d83d5089f642ac2aef
Vahid Akhtar <akhtarvahid543@gmail.com>
2024-08-20 20:58:03 +0530

    zoom about image on click

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,40 +5,57 @@ import aboutImg from "../../images/aboutBcg.jpeg";
 import { PrimaryBtn } from "../common/Buttons";
 import { AboutCenter } from "./About.styled";
 import { TooltipBox, TooltipCard, TooltipText } from "../common/Tooltip";
+import Zoom from "react-medium-image-zoom";
+import "react-medium-image-zoom/dist/styles.css";
 
 const About = () => {
   const [show, setShow] = useState(false);
+  const [isClickedToZoom, setIsClickedToZoom] = useState(false);
   return (
-    <Section>
-      <AboutCenter>
-        <div className="about-img" onClick={()=> console.log('zoom in')}>
-          <img src={aboutImg} alt="about us" />
-        </div>
-        <div className="about-info">
-          <Title title="about us" />
-          <p>
-            From its personalised service and sophisticated cuisine to its
-            distinctive design, authentic culture and calming spa, our Maldives
-            luxury resort works hard to tailor the experience to your needs. We
-            can orchestrate customised cruises to neighbouring islands and
-            beaches; plan private dinners at secluded locations; arrange yoga,
-            diving or snorkelling; and create adventures for families travelling
-            together.
-          </p>
+    <>
+      <Section>
+        <AboutCenter>
+          <div
+            className="about-img"
+            onClick={() => setIsClickedToZoom(!isClickedToZoom)}
+          >
+            <img src={aboutImg} alt="about us" />
+          </div>
+          <div className="about-info">
+            <Title title="about us" />
+            <p>
+              From its personalised service and sophisticated cuisine to its
+              distinctive design, authentic culture and calming spa, our
+              Maldives luxury resort works hard to tailor the experience to your
+              needs. We can orchestrate customised cruises to neighbouring
+              islands and beaches; plan private dinners at secluded locations;
+              arrange yoga, diving or snorkelling; and create adventures for
+              families travelling together.
+            </p>
 
-          <TooltipCard>
-            <TooltipText>
-              <PrimaryBtn onMouseEnter={() => setShow(true)}>
-                read more
-              </PrimaryBtn>
-            </TooltipText>
-            <TooltipBox>
-              <p>{show ? "Coming soon" : "Read more..."}</p>
-            </TooltipBox>
-          </TooltipCard>
-        </div>
-      </AboutCenter>
-    </Section>
+            <TooltipCard>
+              <TooltipText>
+                <PrimaryBtn onMouseEnter={() => setShow(true)}>
+                  read more
+                </PrimaryBtn>
+              </TooltipText>
+              <TooltipBox>
+                <p>{show ? "Coming soon" : "Read more..."}</p>
+              </TooltipBox>
+            </TooltipCard>
+          </div>
+        </AboutCenter>
+      </Section>
+      {isClickedToZoom && (
+        <Zoom>
+          <img
+            alt="That Wanaka Tree, New Zealand by Laura Smetsers"
+            src={aboutImg}
+            width="500"
+          />
+        </Zoom>
+      )}
+    </>
   );
 };
 
