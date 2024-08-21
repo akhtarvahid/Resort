396179754f4dd42da0f999bd72e6260760e30674
Vahid Akhtar <akhtarvahid543@gmail.com>
2024-08-21 20:29:45 +0530

    zoomed image in center

--- src/components/About/ZoomedImage.js
+++ src/components/About/ZoomedImage.js
@@ -1,15 +1,26 @@
-import React from "react";
-import Zoom from "react-medium-image-zoom";
-import "react-medium-image-zoom/dist/styles.css";
+import React, { useCallback, useState } from 'react'
+import { Controlled as ControlledZoom } from 'react-medium-image-zoom'
+import 'react-medium-image-zoom/dist/styles.css'
+import { InnerZoomBox } from './About.styled'
 
 export default function ZoomedImage({ aboutImg }) {
+  const [isZoomed, setIsZoomed] = useState(false)
+
+  const handleZoomChange = useCallback(shouldZoom => {
+    setIsZoomed(shouldZoom)
+  }, [])
   return (
-    <Zoom>
+    <div className="wrapper">
+    <InnerZoomBox>
+       <ControlledZoom isZoomed={isZoomed} onZoomChange={handleZoomChange}>
       <img
-        alt="That Wanaka Tree, New Zealand by Laura Smetsers"
+        alt="That wanaka tree, alone in the water near mountains"
         src={aboutImg}
-        width="500"
+        width="960"
       />
-    </Zoom>
+    </ControlledZoom>
+    </InnerZoomBox>
+    </div>
+   
   );
 }
